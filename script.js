24356b693e9f9c084abc0920d5f45ad797683c67
swag31415 <kidswagatam18@gmail.com>
2021-06-18 12:53:34 -0400

    Added the basics of push interface

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-M.toast({html: 'Welcome!'})
+const toast = (msg, classes = "") => M.toast({html: msg, classes: classes})
+const succ = (msg) => toast(msg, "green")
+const fail = (msg) => toast(msg, "red")
+
+succ("Welcome!")
+
+function hide_opts(should_hide) {
+  document.getElementById("opts").style.display = should_hide ? "none" : ""
+}
+
+const push = document.getElementById("push")
+function start_push() {
+  hide_opts(true)
+  push.style.display = ""
+}
+
+const scanned_push = document.getElementById("scanned_push")
+function add_to_push(item) {
+  let panel = document.createElement("p")
+  panel.className = "flow-text center-align col s6 m4 l3"
+  panel.innerText = item
+  scanned_push.prepend(panel)
+  succ(`Added ${item} to push`)
+}
+
+function show_route() {
+  // TODO
+  fail("not yet implemented")
+}
