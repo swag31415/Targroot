f11940b71f5e4dd43360d979df5cbba50950d265
swag31415 <kidswagatam18@gmail.com>
2021-06-18 11:23:08 -0400

    Added proof-of-concept Quagga implementation

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-M.toast({html: 'Welcome!'})
+const toast = (msg) => M.toast({html: msg})
+toast('Welcome!')
+
+Quagga.init({
+  numOfWorkers: 8,
+  inputStream: {
+    name: "Live",
+    type: "LiveStream", 
+    target: "#outp"
+  },
+  decoder: {
+    readers: ["upc_reader", "upc_e_reader", "ean_reader", "ean_8_reader"]
+  },
+  constraints: {
+    width: 3468,
+    height: 4624
+  },
+  locator: {
+    patchsize: "small"
+  }
+}, (err) => {
+  if (err) {
+    toast(err)
+    return
+  }
+  toast("Initialization finished. Ready to start");
+  Quagga.start();
+})
+
+Quagga.onDetected((data) => toast(data.codeResult.code))
